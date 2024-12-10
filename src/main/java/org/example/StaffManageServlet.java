75abcc4a1c4e54797358a76a2e509391f1d1d8bb
jizhilatiao233 <22373345@buaa.edu.cn>
2024-12-10 22:10:36 +0800

    更新：进货信息的后端

--- src/main/java/org/example/StaffManageServlet.java
+++ src/main/java/org/example/StaffManageServlet.java
@@ -1,4 +1,16 @@
 package org.example;
 
-public class StaffManageServlet {
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.gson.Gson;
+
+public class StaffManageServlet extends HttpServlet {
 }
